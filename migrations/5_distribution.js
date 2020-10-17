508899869d26ae78e51b5ed5b7c69ec29297b5ea
lanjian <air.petrichor@gmail.com>
2020-10-17 21:01:15 +0800

    update migrations

--- migrations/5_distribution.js
+++ migrations/5_distribution.js
@@ -25,14 +25,11 @@ const iTokenGov = artifacts.require("iTokenGovernorAlpha");
 const iBTCGov = iETHGov = iUSDGov = iTokenGov;
 
 // deployed fourth
-const AlthorpInNorthamptonshire = artifacts.require("AlthorpInNorthamptonshire");
+
 const BlenheimPalace = artifacts.require("BlenheimPalace");
-const EstateOfCuskovo = artifacts.require("EstateOfCuskovo");
 const RockefellerManor = artifacts.require("RockefellerManor");
 const YasnayaPolyana = artifacts.require("YasnayaPolyana");
 
-// deployed fifth
-const IFAVote = artifacts.require("IFAVote");
 
 // ============ Main Migration ============
 
@@ -74,15 +71,12 @@ async function deployDistribution(deployer, network, accounts) {
     let iethGov = await iETHGov.deployed();
     let ibtcGov = await iBTCGov.deployed();
 
-    await deployer.deploy(AlthorpInNorthamptonshire);
+
     await deployer.deploy(BlenheimPalace);
-    await deployer.deploy(EstateOfCuskovo);
     await deployer.deploy(RockefellerManor);
     await deployer.deploy(YasnayaPolyana);
 
-    let althorpInNorthamptonshire = new web3.eth.Contract(AlthorpInNorthamptonshire.abi, AlthorpInNorthamptonshire.address);
     let blenheimPalace = new web3.eth.Contract(BlenheimPalace.abi, BlenheimPalace.address);
-    let estateOfCuskovo = new web3.eth.Contract(EstateOfCuskovo.abi, EstateOfCuskovo.address);
     let rockefellerManor = new web3.eth.Contract(RockefellerManor.abi, RockefellerManor.address);
     let yasnayaPolyana = new web3.eth.Contract(YasnayaPolyana.abi, YasnayaPolyana.address);
 
@@ -90,18 +84,14 @@ async function deployDistribution(deployer, network, accounts) {
     // set rewardVote(IFAVote address)
     console.log("setting rewardVote");
     await Promise.all([
-        althorpInNorthamptonshire.methods.setRewardVote(IFAVote.address),
         blenheimPalace.methods.setRewardVote(IFAVote.address),
-        estateOfCuskovo.methods.setRewardVote(IFAVote.address),
         rockefellerManor.methods.setRewardVote(IFAVote.address),
         yasnayaPolyana.methods.setRewardVote(IFAVote.address),
     ]);
 
     console.log("setting distributor");
     await Promise.all([
-        althorpInNorthamptonshire.methods.setRewardDistribution(accounts[0]).send({from: accounts[0], gas: 100000}),
         blenheimPalace.methods.setRewardDistribution(accounts[0]).send({from: accounts[0], gas: 100000}),
-        estateOfCuskovo.methods.setRewardDistribution(accounts[0]).send({from: accounts[0], gas: 100000}),
         rockefellerManor.methods.setRewardDistribution(accounts[0]).send({from: accounts[0], gas: 100000}),
         yasnayaPolyana.methods.setRewardDistribution(accounts[0]).send({from: accounts[0], gas: 100000}),
     ]);
@@ -109,59 +99,39 @@ async function deployDistribution(deployer, network, accounts) {
 
     console.log("transfering and notifying");
     await Promise.all([
-        // ifa.addMinter(AlthorpInNorthamptonshire.address),
-        iusd._setIncentivizer(AlthorpInNorthamptonshire.address),
-
-        // ifa.addMinter(BlenheimPalace.address),
+        iusd.addMinter(BlenheimPalace.address),
         iusd._setIncentivizer(BlenheimPalace.address),
 
-        // ifa.addMinter(EstateOfCuskovo.address),
-        iusd._setIncentivizer(EstateOfCuskovo.address),
+        ibtc.addMinter(YasnayaPolyana.address),
+        ibtc._setIncentivizer(YasnayaPolyana.address),
 
-        // ifa.addMinter(EstateOfCuskovo.address),
+        ieth.addMinter(RockefellerManor.address),
         ieth._setIncentivizer(RockefellerManor.address),
 
-        // ifa.addMinter(YasnayaPolyana.address),
-        ibtc._setIncentivizer(YasnayaPolyana.address),
 
     ]);
     await Promise.all([
         // pools is a minter and prepopulates itself.
-        althorpInNorthamptonshire.methods.notifyRewardAmount("0"),
         blenheimPalace.methods.notifyRewardAmount("0"),
-        estateOfCuskovo.methods.notifyRewardAmount("0"),
         rockefellerManor.methods.notifyRewardAmount("0"),
         yasnayaPolyana.methods.notifyRewardAmount("0"),
-
-        // althorpInNorthamptonshire.methods.notifyRewardAmount("0").send({from: accounts[0], gas: 100000}),
-        // blenheimPalace.methods.notifyRewardAmount("0").send({from: accounts[0], gas: 100000}),
-        // estateOfCuskovo.methods.notifyRewardAmount("0").send({from: accounts[0], gas: 100000}),
-        // rockefellerManor.methods.notifyRewardAmount("0").send({from: accounts[0], gas: 100000}),
-        // yasnayaPolyana.methods.notifyRewardAmount("0").send({from: accounts[0], gas: 100000}),
     ]);
 
 
-    // transfer RewardDistribution Ownership to IFA Timelock
+    // transfer RewardDistribution Ownership to Timelock
     /*
     await Promise.all([
-        althorpInNorthamptonshire.methods.setRewardDistribution(IFATimelock.address).send({
-            from: accounts[0],
-            gas: 100000
-        }),
-        blenheimPalace.methods.setRewardDistribution(IFATimelock.address).send({from: accounts[0], gas: 100000}),
-        estateOfCuskovo.methods.setRewardDistribution(IFATimelock.address).send({from: accounts[0], gas: 100000}),
-        rockefellerManor.methods.setRewardDistribution(IFATimelock.address).send({from: accounts[0], gas: 100000}),
-        yasnayaPolyana.methods.setRewardDistribution(IFATimelock.address).send({from: accounts[0], gas: 100000}),
+        blenheimPalace.methods.setRewardDistribution(iUSDTimelock.address).send({from: accounts[0], gas: 100000}),
+                yasnayaPolyana.methods.setRewardDistribution(iBTCTimelock.address).send({from: accounts[0], gas: 100000}),
+        rockefellerManor.methods.setRewardDistribution(iETHimelock.address).send({from: accounts[0], gas: 100000}),
     ]);
 
 
-    // transfer Pool Ownership to IFA Timelock
+    // transfer Pool Ownership to Timelock
     await Promise.all([
-        althorpInNorthamptonshire.methods.transferOwnership(IFATimelock.address).send({from: accounts[0], gas: 100000}),
-        blenheimPalace.methods.transferOwnership(IFATimelock.address).send({from: accounts[0], gas: 100000}),
-        estateOfCuskovo.methods.transferOwnership(IFATimelock.address).send({from: accounts[0], gas: 100000}),
-        rockefellerManor.methods.transferOwnership(IFATimelock.address).send({from: accounts[0], gas: 100000}),
-        yasnayaPolyana.methods.transferOwnership(IFATimelock.address).send({from: accounts[0], gas: 100000}),
+        blenheimPalace.methods.transferOwnership(iUSDTimelock.address).send({from: accounts[0], gas: 100000}),
+        yasnayaPolyana.methods.transferOwnership(iBTCTimelock.address).send({from: accounts[0], gas: 100000}),
+        rockefellerManor.methods.transferOwnership(iETHimelock.address).send({from: accounts[0], gas: 100000}),
     ]);
     */
 
@@ -173,13 +143,14 @@ async function deployDistribution(deployer, network, accounts) {
         iusdReserves._setPendingGov(iUSDTimelock.address),
         iusdRebaser._setPendingGov(iUSDTimelock.address),
 
+        ibtc._setPendingGov(iBTCTimelock.address),
+        ibtcReserves._setPendingGov(iBTCTimelock.address),
+        ibtcRebaser._setPendingGov(iBTCTimelock.address),
+
         ieth._setPendingGov(iETHTimelock.address),
         iethReserves._setPendingGov(iETHTimelock.address),
         iethRebaser._setPendingGov(iETHTimelock.address),
 
-        ibtc._setPendingGov(iBTCTimelock.address),
-        ibtcReserves._setPendingGov(iBTCTimelock.address),
-        ibtcRebaser._setPendingGov(iBTCTimelock.address),
 
     ]);
 
@@ -211,6 +182,31 @@ async function deployDistribution(deployer, network, accounts) {
             0
         ),
 
+        // iBTC
+        ibtcTimelock.executeTransaction(
+            iBTCProxy.address,
+            0,
+            "_acceptGov()",
+            "0x",
+            0
+        ),
+
+        ibtcTimelock.executeTransaction(
+            iBTCReserves.address,
+            0,
+            "_acceptGov()",
+            "0x",
+            0
+        ),
+
+        ibtcTimelock.executeTransaction(
+            iBTCRebaser.address,
+            0,
+            "_acceptGov()",
+            "0x",
+            0
+        ),
+
 
         // iETH
         iethTimelock.executeTransaction(
@@ -236,43 +232,20 @@ async function deployDistribution(deployer, network, accounts) {
             "0x",
             0
         ),
-
-        // iBTC
-        ibtcTimelock.executeTransaction(
-            iBTCProxy.address,
-            0,
-            "_acceptGov()",
-            "0x",
-            0
-        ),
-
-        ibtcTimelock.executeTransaction(
-            iBTCReserves.address,
-            0,
-            "_acceptGov()",
-            "0x",
-            0
-        ),
-
-        ibtcTimelock.executeTransaction(
-            iBTCRebaser.address,
-            0,
-            "_acceptGov()",
-            "0x",
-            0
-        ),
     ]);
 
     await iusdTimelock.setPendingAdmin(iUSDGov.address);
     await iusdGov.__acceptAdmin();
     await iusdGov.__abdicate();
 
+    await ibtcTimelock.setPendingAdmin(iBTCGov.address);
+    await ibtcGov.__acceptAdmin();
+    await ibtcGov.__abdicate();
+
     await iethTimelock.setPendingAdmin(iETHGov.address);
     await iethGov.__acceptAdmin();
     await iethGov.__abdicate();
 
-    await ibtcTimelock.setPendingAdmin(iBTCGov.address);
-    await ibtcGov.__acceptAdmin();
-    await ibtcGov.__abdicate();
+
     */
 }
